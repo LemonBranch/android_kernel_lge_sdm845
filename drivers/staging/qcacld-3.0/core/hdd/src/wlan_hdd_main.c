07a4064796c6ee208a874bb879e7eab211d707df
Diab Neiroukh <lazerl0rd@thezest.dev>
2020-08-04 14:36:34 +0100

    ffffkjk
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -7566,6 +7566,7 @@ void wlan_hdd_deinit_tx_rx_histogram(hdd_context_t *hdd_ctx)
 	hdd_ctx->hdd_txrx_hist = NULL;
 }
 
+#ifdef WLAN_DEBUG
 static uint8_t *convert_level_to_string(uint32_t level)
 {
 	switch (level) {
@@ -7582,7 +7583,7 @@ static uint8_t *convert_level_to_string(uint32_t level)
 		return "INVAL";
 	}
 }
-
+#endif
 
 /**
  * wlan_hdd_display_tx_rx_histogram() - display tx rx histogram
